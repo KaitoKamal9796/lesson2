510e12e4d0578bd98da234d8bad5665b9f8b05ac
KaitoKamal9796 <59758775+KaitoKamal9796@users.noreply.github.com>
2020-03-11 16:24:27 +0800

    Laravel TItle

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel HUHU</title>
+        <title>Laravel HUHU JAAHA</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
